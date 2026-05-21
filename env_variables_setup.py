6b16915af0a29d6c18287bd7d70edc4d6b5b0223
Ankur-ipynb <ankur3103verma@gmail.com>
2026-05-21 11:41:49 +0530

    Replace hardcoded paths with placeholders in setup
    Updated environment variable paths to placeholders for user configuration.

--- env_variables_setup.py
+++ env_variables_setup.py
@@ -8,10 +8,10 @@ def initialize_spark():
     logging.info("Python executable: " + sys.executable)
     logging.info("sys.path: " + str(sys.path))
 
-    os.environ['SPARK_HOME'] = 'D:/spark'
-    os.environ['HADOOP_HOME'] = 'D:/hadoop'
-    os.environ['JAVA_HOME'] = 'D:/java'
-    os.environ['PATH'] = os.environ['PATH'] + ';' + 'D:/hadoop/bin'
+    os.environ['SPARK_HOME'] = '<YOUR SPARK PATH>'
+    os.environ['HADOOP_HOME'] = '<YOUR HADOOP PATH>'
+    os.environ['JAVA_HOME'] = '<YOUR JAVA PATH>'
+    os.environ['PATH'] = os.environ['PATH'] + ';' + '<YOUR HADOOP BIN PATH>'
     logging.info("Environment variables set.")
 
     winutils_path = 'D:/hadoop/bin/winutils.exe'
@@ -22,4 +22,4 @@ def initialize_spark():
 
 
 def get_servicenow_auth():
-    return HTTPBasicAuth("admin", "X-Xi4Rg9Wnj*")
+    return HTTPBasicAuth("admin", "<YOUR SERVICE-NOW PDI PASSWORD>")
